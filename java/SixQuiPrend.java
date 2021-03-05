0ad913b893af8200b75b5cab57bfbd9262095e9f
E.BARBOSA <ebarbosa4@myges.fr>
2021-03-05 19:46:37 +0100

    6Quiprend_done

--- java/SixQuiPrend.java
+++ java/SixQuiPrend.java
@@ -5,9 +5,15 @@ public class SixQuiPrend {
     private static final int NB_CARD_MAX = 104;
 
     public static int findNumberOfBullHeads(int value) {
-        // TODO
-
-        return 0;
+        if(value == 55) {
+            return 7;
+        }else if(value % 11 == 0){
+            return 5;
+        }else if(value % 10 == 0){
+            return 3;
+        }else if(value % 5 == 0) {
+            return 2;
+        }else return 1;
     }
 
     public static void gameLoop() {
